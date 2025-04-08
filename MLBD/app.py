540391db65e0a4bb2bd6a09692570e09874f7714
Ankit Kumar <91029424+ankitkumar3440@users.noreply.github.com>
2025-04-08 22:24:28 +0530

    Update app.py

--- MLBD/app.py
+++ MLBD/app.py
@@ -19,8 +19,7 @@ from sentence_transformers import SentenceTransformer
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 import numpy as np
-import spacy
-nlp = spacy.load("en_core_web_sm")
+
 
 
 # Download required NLTK resources
